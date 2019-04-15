94e7bfed36ada9fb0b554c47152b2b564170ab00
yourname <zhangyuelun123@sina.com>
2019-04-15 16:32:02 +0800

    初始化+固定头和尾

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+      <h1 class="top">这是根组件</h1>
+      <!--  固定顶部 -->
+      <mt-header fixed title="固定在顶部"></mt-header>
+
+
+      <!-- 固定底部 -->
+	    <nav class="mui-bar mui-bar-tab">
+        <a class="mui-tab-item mui-active" href="#tabbar">
+          <span class="mui-icon mui-icon-home"></span>
+          <span class="mui-tab-label">首页</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-chat">
+          <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+          <span class="mui-tab-label">消息</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-contact">
+          <span class="mui-icon mui-icon-contact"></span>
+          <span class="mui-tab-label">通讯录</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-map">
+          <span class="mui-icon mui-icon-gear"></span>
+          <span class="mui-tab-label">设置</span>
+        </a>
+		  </nav>
+
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+ .top{
+   margin-top: 40px;
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+
 import router from './router'
 
+import './lib/mint-ui/style.css'
+import './lib/mui/css/mui.min.css'
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+// import './lib/mint-ui/mui.min.css'
+
+// import './lib/mint-ui/index.js'
+// 按需引入部分组件
+// 固定顶部
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
+  render: h => h(App)
 }).$mount('#app')
